aec0354f80cf3b5169fc4cc99c49b8ae6b68cd9f
Vignesh BUI0094 <amvignesh@brightuitechnologies.com>
2024-12-20 13:02:42 +0530

    Learn/Vue js local setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="small-container">
+    <h1>Employees</h1>
+    <employee-form @add:employee="addEmployee" />
+    <employee-table :employees="employees" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import EmployeeTable from './components/EmployeeTable.vue';
+import EmployeeForm from './components/EmployeeForm.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EmployeeTable,
+    EmployeeForm
+  },
+  data(){
+    return{
+      employees:[
+        {
+          id : '1',
+          name : 'Richerd',
+          email : 'R@gmail.com'
+        },
+         {
+           id : '2',
+          name : 'Watson',
+          email : 'W@gmail.com'
+        },
+         {
+           id : '3',
+          name : 'Jimmy',
+          email : 'J@gmail.com'
+        }
+      ]
+    }
+  },
+  methods:{
+    addEmployee(employee){
+        const lastId = this.employees.length > 0 ?this.employees[this.employees.length-1].id : 0 ;
+        const id = (parseInt(lastId) + 1);
+        const newEmployee = {...employee,id};
+        this.employees = [...this.employees,newEmployee];
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  button{
+    background: #009435;
+    border: 1px solid #009435;
+  }
+  .small-container{
+    max-width: 680px;
+  }
 </style>
