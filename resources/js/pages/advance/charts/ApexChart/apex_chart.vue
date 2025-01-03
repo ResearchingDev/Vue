a8ef1814d184073a388678f899dd7cc25fba20af
Prakash-BUI0097 <prakash@brightuitechnologies.com>
2025-01-03 11:57:23 +0530

    Vue - Dev -  apexchart vue file

---
+++ resources/js/pages/advance/charts/ApexChart/apex_chart.vue
@@ -0,0 +1,29 @@
+<template>
+<Breadcrumbs main="Charts" title="ApexChart" />
+<div class="container-fluid ">
+    <div class="row">
+        <barChart />
+        <columnChart />
+        <columnChart2 />
+        <pieChart />
+        <donutChart />
+    </div>
+</div>
+</template>
+
+<script>
+import barChart from "./barChart.vue"
+import columnChart from "./columnChart.vue"
+import columnChart2 from "./columnChart2.vue"
+import pieChart from "./pieChart.vue"
+import donutChart from "./donutChart.vue"
+export default {
+    components: {
+        barChart,
+        columnChart,
+        columnChart2,
+        pieChart,
+        donutChart,
+    }
+};
+</script>
