6d3e3137816af43ed5f03c5a4cde5c4a3b839635
Prakash-BUI0097 <prakash@brightuitechnologies.com>
2025-01-07 12:24:56 +0530

    Vue - Dev- User Index view pages updation

---
+++ resources/js/pages/users/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <Breadcrumbs title="Users" main="Manage" />
+    <div class="container-fluid ">
+        <div class="clients">
+            <div class="row">
+                <tableList />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import tableList from './tableList.vue';
+
+export default {
+    name: 'clients',
+    components: {
+        tableList
+    },
+
+}
+</script>
